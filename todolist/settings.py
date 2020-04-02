d36c482c435d8fba7a273697c0a26a356439a5fe
CortesBeringola <rafael.cortes.beringola@gmail.com>
2020-04-02 00:07:10 -0500

    Added files and changes required for deployment to heroku

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,10 +20,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c7^7#$=pa6-fr3i9d3u$$wo_#zwq^l&b8x*k#+^lo4nvlov&zb'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c7^7#$=pa6-fr3i9d3u$$wo_#zwq^l&b8x*k#+^lo4nvlov&zb')
+#SECRET_KEY = 'c7^7#$=pa6-fr3i9d3u$$wo_#zwq^l&b8x*k#+^lo4nvlov&zb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = (os.path.join(BASE_DIR,'static'))
+STATIC_ROOT = (os.path.join(BASE_DIR,'static'))
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
