1bae2993f3039db54a9d0411d54191531bd047d0
CortesBeringola <rafael.cortes.beringola@gmail.com>
2020-04-02 00:22:08 -0500

    Added files and changes required for deployment to heroku

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c7^7#$=pa6-fr3i9d3u$$wo_#zwq^l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personaltasksassistant.herokuapp.com','127.0.0.1']
 
 
 # Application definition
